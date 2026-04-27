5f07d0bb2934f0ce9b0ab53490ff519cc1955666
Sam Kikoma <kikomasamson@gmail.com>
2026-04-27 21:01:30 +0200

    feat: add app entry point

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("setup");
+import { createBatches } from "./batch.js";
+import ExternalService from "./externalService.js";
+import { parseXML } from "./xmlParser.js";
+
+export async function main() {
+	const filePath = "./feed.xml";
+
+	const products = await parseXML(filePath);
+	console.log(`Total valid products: ${products.length}\n`);
+
+	const batches = createBatches(products);
+	console.log(`Created ${batches.length} batches\n`);
+
+	const service = ExternalService();
+
+	for (const batch of batches) {
+		service.call(batch);
+	}
+}
+
+main();
